2dd778356ac681c57d8c2d3bc989b24602751aa0
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2025-09-26 18:23:46 +0100

    Exercise 6.22 Implemented

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,11 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from '../requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from '../requests'
 
 const AnecdoteList = () => {
+  const queryClient = useQueryClient()
+
   const { data: anecdotes, isLoading, isError } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
-    retry: false  // or retry: 1
+    retry: false,
+  })
+
+  const voteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      // update cache directly
+      queryClient.setQueryData(['anecdotes'], (old) =>
+        old.map((a) => (a.id === updatedAnecdote.id ? updatedAnecdote : a))
+      )
+    },
   })
 
   if (isLoading) {
@@ -16,14 +28,21 @@ const AnecdoteList = () => {
     return <div>anecdote service not available due to problems in server</div>
   }
 
+  const handleVote = (anecdote) => {
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
+
   return (
     <div>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
-          <div>has {anecdote.votes}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
